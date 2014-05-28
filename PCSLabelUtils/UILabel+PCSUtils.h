1058d900a3d6deedf3e3762c92edccd65cc20ea1
Peter Chen <pdchen@gmail.com>
2014-05-28 09:46:56 -0400

    add project

---
+++ PCSLabelUtils/UILabel+PCSUtils.h
@@ -0,0 +1,14 @@
+//====================================================================================================
+// Author: Peter Chen
+// Created: 5/28/14
+// Copyright 2014 Peter Chen
+//====================================================================================================
+
+#import <Foundation/Foundation.h>
+
+
+@interface UILabel (PCSUtils)
+
+- (void)resizeToFitVerticallyWithMaxHeight:(int)maxHeight;
+
+@end
---
+++ PCSLabelUtils/UILabel+PCSUtils.m
@@ -0,0 +1,22 @@
+//====================================================================================================
+// Author: Peter Chen
+// Created: 5/28/14
+// Copyright 2014 Peter Chen
+//====================================================================================================
+
+#import "UILabel+PCSUtils.h"
+
+
+@implementation UILabel (PCSUtils)
+
+- (void)resizeToFitVerticallyWithMaxHeight:(int)maxHeight {
+   NSStringDrawingOptions options = self.numberOfLines > 1 ? NSStringDrawingUsesLineFragmentOrigin : 0;
+   NSDictionary *attributes = @{ NSFontAttributeName : self.font };
+   CGRect textBoundingRect = [self.text boundingRectWithSize:CGSizeMake(self.frame.size.width, maxHeight) options:options attributes:attributes context:nil];
+   
+   CGRect frame = self.frame;
+   frame.size.height = textBoundingRect.size.height;
+   self.frame = frame;
+}
+
+@end
